14a173d6299af3f20025e4d2e3bb270606750cbd
SSzSun <jaruphat.sun@gmail.com>
2023-10-22 12:04:55 +0700

    Update ENG Plc

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import numpy as np
 import cv2
 
 from sort.sort import *
-from util import get_car, read_license_plate
+from util import get_car, read_license_plate, write_csv
 
+
+results = {}
 
 mot_tracker = Sort()
 coco_model = YOLO('yolov8n.pt')
@@ -23,7 +25,9 @@ while ret:
     frame_nmr += 1
     ret, frame = cap.read()
     if ret:
-        # results[frame_nmr] = {}
+        if frame_nmr > 10:
+           break
+        results[frame_nmr] = {}
         # detect vehicles
         detections = coco_model(frame)[0]
         detections_ = []
@@ -46,7 +50,7 @@ while ret:
             # crop license plate
             # license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
 
-            # # process license plate
+            # process license plate
             # license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
             # _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
@@ -66,12 +70,12 @@ while ret:
                 # read license plate number
                 license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
 
-#                 if license_plate_text is not None:
-#                     results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
-#                                                   'license_plate': {'bbox': [x1, y1, x2, y2],
-#                                                                     'text': license_plate_text,
-#                                                                     'bbox_score': score,
-#                                                                     'text_score': license_plate_text_score}}
+                if license_plate_text is not None:
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                                    'license_plate': {'bbox': [x1, y1, x2, y2],
+                                                                    'text': license_plate_text,
+                                                                    'bbox_score': score,
+                                                                    'text_score': license_plate_text_score}}
 
-# # write results
-# write_csv(results, './test.csv')
+# write results
+write_csv(results, './test.csv')
